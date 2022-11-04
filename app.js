c384035a619234455181a34c5b8b6c114ac3ca50
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-04 18:49:59 +0100

    code added

---
+++ app.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const cityName = document.querySelector(".city-name");
+const warning = document.querySelector(".warning");
+const photo = document.querySelector(".photo");
+const weather = document.querySelector(".weather");
+const temperature = document.querySelector(".temperature");
+const humidity = document.querySelector(".humidity");
+

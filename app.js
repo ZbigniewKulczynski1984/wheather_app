a28e129a9a109557134ca3484d392d26a200a5b7
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-04 18:51:25 +0100

    code added

--- app.js
+++ app.js
@@ -7,3 +7,6 @@ const weather = document.querySelector(".weather");
 const temperature = document.querySelector(".temperature");
 const humidity = document.querySelector(".humidity");
 
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const API_KEY = "&appid=52d2be5f0d491740497f4366e9a06006";
+const API_UNITS = "&units=metric";
